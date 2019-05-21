8d549eeee3f05f8802580c9fd3bb16c105d3b776
caroline <caroline@debian.caroline>
2019-05-21 02:38:20 -0300

    working but just one validation

--- temperature/views.py
+++ temperature/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import re
+
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,15 +13,23 @@ from darksky import get_url, make_api_call
 def home(request):
     return render(request, "temperature/home.htm")
 
+
 @csrf_exempt
 def post_temperature(request):
+    regex = re.compile(r'^[-\d]{2,3}|[\d]{2}$')
+    errors = []
     if request.method == 'POST':
         latitude = request.POST.get('latitude')
         longitude = request.POST.get('longitude')
-        post_url = get_url(latitude, longitude)
-        celsius = make_api_call(post_url)
-        return render(request, 'temperature/home.htm', {
-            "lat": latitude,
-            "long": longitude,
-            "celsius": celsius
-        })
+        if not latitude or not longitude:
+            errors.append('Please enter a latitude and longitude value.')
+            #adicionar condicao caso nao de match com o regex
+        else:
+            post_url = get_url(latitude, longitude)
+            celsius = make_api_call(post_url)
+            return render(request, 'temperature/home.htm', {
+                "lat": latitude,
+                "long": longitude,
+                "celsius": celsius
+            })
+        return render(request, 'temperature/home.htm', {'errors': errors})
